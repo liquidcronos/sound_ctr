dad48c00139f969e34d6fd6aaf64494969533cbd
Jan Baumgärtner <22688144+liquidcronos@users.noreply.github.com>
2021-12-01 15:28:33 +0100

    implemented control system update with state, fixed #1

--- src/sound_player_test.py
+++ src/sound_player_test.py
@@ -4,44 +4,50 @@ import numpy as np
 
 
 class sound_game:
-    def __init__(self, set_point, ctr_interval: Tuple):
-        self.sample_rate = 44100
-        self.amp = 4096
+    def __init__(self, set_point, ctr_interval: Tuple, initial_state=0):
+        self._sample_rate = 44100
+        self._amp = 4096
 
         self.set_point = set_point
         self.ctr_interval = ctr_interval
         self.ctr_action = ctr_interval[0]
+        self.state = initial_state
 
-        pygame.mixer.init(frequency=self.sample_rate,
+        pygame.mixer.init(frequency=self._sample_rate,
                           size=-16, channels=2, buffer=2**12)
 
-        buf = np.zeros((44100, 2), dtype=np.int16)
+        self.buffer_length = int(44100 * 0.25)
+        buf = np.zeros((self.buffer_length, 2), dtype=np.int16)
         sound = pygame.sndarray.make_sound(buf)
         self.buf = pygame.sndarray.samples(sound)
 
         sound.play(-1)
 
     def set_sound_buffer(self, left_freq, right_freq):
-        for i in range(0, self.sample_rate):
-            self.buf[i][0] = self.amp*np.sin(2 * np.pi * i *
-                                             left_freq / self.sample_rate)
-            self.buf[i][1] = self.amp*np.sin(2 * np.pi * i *
-                                             right_freq / self.sample_rate)
+        for i in range(0, self.buffer_length):
+            self.buf[i][0] = self._amp*np.sin(2 * np.pi * i *
+                                              left_freq / self._sample_rate)
+            self.buf[i][1] = self._amp*np.sin(2 * np.pi * i *
+                                              right_freq / self._sample_rate)
 
     def change_set_point(self, new_set_point):
         self.set_point = new_set_point
         self.set_sound_buffer(self.set_point, self.ctr_action)
 
+    def system_update(self, ctr_action):
+        self.state = self.state + ctr_action
+
     def input_ctr_action(self, new_ctr_action):
         if not (new_ctr_action >= self.ctr_interval[0]) or not (new_ctr_action <= self.ctr_interval[1]):
             raise ValueError('input not within the control interval')
         self.ctr_action = new_ctr_action
-        self.set_sound_buffer(self.set_point, self.ctr_action)
+        self.system_update(self.ctr_action)
+        self.set_sound_buffer(self.set_point, self.state)
 
 
-test_class = sound_game(0, (350, 500))
-pygame.time.wait(int(20))
+test_class = sound_game(0, (-100, 100), initial_state=400)
+pygame.time.wait(int(40))
 test_class.change_set_point(440)
-for i in range(20):
-    test_class.input_ctr_action(380+10*i)
-    pygame.time.wait(int(10))
+for i in range(80):
+    test_class.input_ctr_action(20)
+    pygame.time.wait(1)
