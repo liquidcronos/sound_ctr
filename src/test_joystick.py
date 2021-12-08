9721926e1e1422c32605accb91417c63b38bc959
Jan Baumgärtner <22688144+liquidcronos@users.noreply.github.com>
2021-12-08 08:48:26 +0100

    add test using joystick

---
+++ src/test_joystick.py
@@ -0,0 +1,22 @@
+import pygame
+from pygame.constants import JOYAXISMOTION
+from sound_game import SoundGame
+
+
+if __name__ == "__main__":
+    pygame.init()
+    pygame.joystick.init()
+    joystick = pygame.joystick.Joystick(0)
+    test_class = SoundGame(0, (-100, 100), initial_state=400)
+    pygame.time.wait(int(40))
+    test_class.change_set_point(440)
+
+    ctr_action = 0
+    while True:
+        for event in pygame.event.get():
+            if event.type == JOYAXISMOTION:
+                if event.axis == 1:  # axis 1 is forward/backward movement
+                    ctr_action = event.value*5.
+
+        test_class.input_ctr_action(ctr_action)
+        pygame.time.wait(1)
