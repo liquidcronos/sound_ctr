00e8589c3f2c0fa950d59e00c9b20b62ec3390a2
Jan Baumgärtner <22688144+liquidcronos@users.noreply.github.com>
2021-12-08 09:26:21 +0100

    added class for easier joystick interaction for later integrating in ROS2

---
+++ src/joystick_sound_game.py
@@ -0,0 +1,20 @@
+from typing import Tuple
+import pygame
+from pygame.constants import JOYAXISMOTION
+from sound_game import SoundGame
+
+
+class JoystickSoundGame(SoundGame):
+    def __init__(self, set_point: float, ctr_interval: Tuple, initial_state=0):
+
+        self._joystick_gain = 5
+        self._joystick_input = 0
+        SoundGame.__init__(self, set_point, ctr_interval, initial_state)
+
+    def update_joystick_input(self, events):
+        for event in events:
+            if event.type == JOYAXISMOTION:
+                if event.axis == 1:  # axis 1 is forward/backward movement
+                    self._joystick_input = event.value*self._joystick_gain
+
+        self.input_ctr_action(self._joystick_input)
--- src/test_joystick.py
+++ src/test_joystick.py
@@ -1,24 +1,17 @@
 import pygame
 from pygame.constants import JOYAXISMOTION
-from sound_game import SoundGame
+from joystick_sound_game import JoystickSoundGame
 
 
 if __name__ == "__main__":
     pygame.init()
     pygame.joystick.init()
     joystick = pygame.joystick.Joystick(0)
-    test_class = SoundGame(0, (-100, 100), initial_state=400)
-    pygame.time.wait(int(40))
-    test_class.change_set_point(440)
 
-    ctr_action = 0
+    test_class = JoystickSoundGame(440, (-100, 100), initial_state=400)
+
     while True:
-        for event in pygame.event.get():
-            if event.type == JOYAXISMOTION:
-                if event.axis == 1:  # axis 1 is forward/backward movement
-                    ctr_action = event.value*5.
-
-        test_class.input_ctr_action(ctr_action)
+        test_class.update_joystick_input(pygame.event.get())
         ctr_state = test_class.get_ctr_state()
         print(ctr_state['error'])
         pygame.time.wait(1)
